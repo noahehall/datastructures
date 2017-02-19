fcb5a710eb79c328efb67d03c0668b6aaeef6ff6
Noah Edward Hall <noahedwardhall@gmail.com>
2017-02-18 22:37:14 -0800

    circular linked list savepoint

--- src/datastructures/lists/circularlinkedlist.js
+++ src/datastructures/lists/circularlinkedlist.js
@@ -9,6 +9,12 @@ export default class CircularLinkedList extends LinkedList {
 
   // CREATE
   // READ
+  find = (item, search = this.head) => {
+    if (!this.length || search.next == this.head) return false;
+    else if (search.element === item) return search;
+    else return this.find(item, search.next);
+  }
+
   findLast = (item = this.current) => {
     return item.next !== this.head
       ? this.findLast(item.next)
@@ -21,6 +27,7 @@ export default class CircularLinkedList extends LinkedList {
     items.push(next.next.element);
     return this.display(items, next.next);
   }
+
   // UPDATE
   // DELETE
 }
